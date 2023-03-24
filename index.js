3472b3bd15dc1dd14e25c6830bfbea0e0fdab7c5
Richard Cole <richard.af.cole@gmail.com>
2023-03-24 15:17:17 -0700

    backend works

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 app.use(express.json())
 
+const cors = require('cors')
+app.use(cors())
+
 var morgan = require('morgan')
 morgan.token('entry', function getEntry (req) {
   console.log('entry', req.body)
@@ -149,30 +152,7 @@ app.delete('/api/persons/:id', (request, response) => {
   response.status(204).end()
 })
 
-// put for swap?
-
-// patch for partial update?
-
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
-
-
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import './index.css';
-// import App from './App';
-// import reportWebVitals from './reportWebVitals';
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
-// // If you want to start measuring performance in your app, pass a function
-// // to log results (for example: reportWebVitals(console.log))
-// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
